bba5429f0e5d754bf55530b24c4bd88814acb082
Owen Deng <chuanqid@smu.edu>
2023-09-08 10:36:04 -0500

    Initial Commit

---
+++ TrioWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrioWeather
+//
+//  Created by Owen on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
